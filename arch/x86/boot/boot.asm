11a5ba451a14b79a62a89e14ec20d95a9e736462
orgofzero <zero.mail@foxmail.com>
2021-02-01 15:35:42 +0800

    Update README.md.

--- arch/x86/boot/boot.asm
+++ arch/x86/boot/boot.asm
@@ -1,28 +1,26 @@
 ;boot.asm
 org 0x7c00
 
-SECTOR_OFFSET equ 1
-SECTOR_COUNT equ 8 ;读入扇区数
-MEMORY_SEG equ 0x7000 ;读入内存基地址
+SECTOR_OFFSET equ 1;起始扇区
+SECTOR_COUNT equ 8;读入扇区数
+MEMORY_SEG equ 0x7000;读入内存基地址
 
 [bits 16]
 entry:
-	mov	ah,0x42
-	mov	dl,0x80
-	mov	si,packet
-	mov	dword[packet+8],SECTOR_OFFSET
-	int	0x13
+	mov ah,0x42
+	mov dl,0x80
+	mov si,packet
+	int 0x13
 	jmp 0x7000:0
 
 packet:
-	db	0x10
-	db	0
-	db	SECTOR_COUNT
-	db	0
-	dw	0
-	dw	MEMORY_SEG
-	dd	0
-	dd	0
+	db 0x10
+	db 0
+	db SECTOR_COUNT
+	db 0
+	dw 0
+	dw MEMORY_SEG
+	dq SECTOR_OFFSET
 
 times 510-($-$$) db 0
 db 0x55,0xaa
--- arch/x86/boot/loader.asm
+++ arch/x86/boot/loader.asm
@@ -9,25 +9,25 @@ KERNEL_ADDR equ 0x100000
 	mov dword[0],0
 	mov dword[4],0
 
-	mov dword[8],0x0000ffff ;4GB,可执行代码段
+	mov dword[8],0x0000ffff;4GB,可执行段
 	mov dword[12],0x00cf9a00
 
-	mov dword[16],0x0000ffff ;4GB,可执行数据段
+	mov dword[16],0x0000ffff;4GB,可读写段
 	mov dword[20],0x00cf9200
 
 	mov ah,0x00
 	mov al,0x03
 	int 0x10
-	cli ;关闭中断
+	cli;关闭中断
 	mov ax,0x7000
 	mov ds,ax
-	lgdt [GDT_addr] ;加载GDTR
+	lgdt [GDT_addr];加载GDTR
 	in al,0x92
 	or al,2
-	out 0x92,al	;enable A20 line
+	out 0x92,al	;启用 A20 line
 	mov eax,cr0
 	or eax,1
-	mov cr0,eax ;cr0 PE位置1
+	mov cr0,eax;cr0 PE位置1
 	jmp dword 0x08:start
 
 [bits 32]
@@ -39,11 +39,11 @@ start:
 	jmp dword 0x08:KERNEL_ADDR
 ;加载内核
 load_kernel:
-	mov cx,11 ;64KB
+	mov cx,11;64KB
 	mov ebx,9
 	mov edx,KERNEL_ADDR
-	call load_block
-	mov cx,0x440
+	call load_block;加载内核到内存
+	mov cx,0x530
 	mov ebx,KERNEL_ADDR+0x1000
 	mov edx,KERNEL_ADDR
 	call memcpy
@@ -55,41 +55,41 @@ load_block:
 	push edx
 	mov dx,0x1f2
 	mov al,cl
-	out dx,al
+	out dx,al;0x1f2端口写加载扇区数
 	mov al,bl
-	add dx,1 ;0x1f3
-	out dx,al ;LBA0~7位
+	add dx,1;0x1f3
+	out dx,al;LBA0~7位
 	shr ebx,8
 	mov al,bl
-	add dx,1 ;0x1f4
-	out dx,al ;LBA8~15位
+	add dx,1;0x1f4
+	out dx,al;LBA8~15位
 	shr ebx,8
 	mov al,bl
-	add dx,1 ;0x1f5
-	out dx,al ;LBA16~23位
+	add dx,1;0x1f5
+	out dx,al;LBA16~23位
 	shr ebx,8
-	and bl,0x0f ;保留低4位
-	or bl,0xe0 ;LBA,主盘
+	and bl,0x0f;保留低4位
+	or bl,0xe0;LBA,主盘
 	mov al,bl
-	add dx,1 ;0x1f6
-	out dx,al ;LBA24~27位
-	mov al,0x20 ;读取磁盘
-	add dx,1 ;0x1f7
+	add dx,1;0x1f6
+	out dx,al;LBA24~27位
+	mov al,0x20;读取磁盘
+	add dx,1;0x1f7
 	out dx,al
 
 	mov ax,cx
 	mov dx,256
-	mul dx
+	mul dx;循环次数=扇区数*512/2
 	mov cx,ax
 	pop edx
 	mov ebx,edx
 	mov dx,0x1f0
 .read_data:
-	call .check_disk
+	call .check_disk;检查磁盘控制器是否可用
 	mov dx,0x1f0
 	in ax,dx
 	mov [ebx],ax
-	add ebx,2
+	add ebx,2;一次读取2-byte
 	loop .read_data
 	ret
 .check_disk:
@@ -99,6 +99,8 @@ load_block:
 	cmp al,0x08
 	jnz .check_disk
 	ret
+;复制内存数据
+;ebx:原始数据地址 edx:目标内存地址
 memcpy:
 	mov ah,[ebx]
 	mov [edx],ah
