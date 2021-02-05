c241c2d56c61a288bcf851c5849725de359ca784
orgofzero <zero.mail@foxmail.com>
2021-02-06 00:49:53 +0800

    putchar函数支持向下滚动屏幕.

--- arch/x86/kernel/graphics.c
+++ arch/x86/kernel/graphics.c
@@ -16,20 +16,39 @@ void print(char *str)
 /* 打印单字符 */
 void putchar(char c)
 {
-	if(c=='\n')//回车
+	/* 回车字符处理 */
+	if(c=='\n')
 	{
 		int t=vga_addr-VGA_ADDR;
 		t-=t%(2*screen_x_size);
 		t+=2*screen_x_size;
 		vga_addr=t+VGA_ADDR;
-		return;
 	}
-	char *p;
-	p=(char*)vga_addr;
-	*p=c;
-	p=(char*)vga_addr+1;
-	*p=(char)0x07;
-	vga_addr+=2;//一个字符占2-byte
+	/* 超出屏幕范围则下滚一行 */
+	if((vga_addr-VGA_ADDR+2)>2*screen_x_size*screen_y_size)
+	{
+		char* i;
+		for(i=(char*)VGA_ADDR;i<(char*)(VGA_ADDR+2*screen_x_size*(screen_y_size-1));i++)
+		{
+			*i=*(i+2*screen_x_size);
+		}
+		/* 填充最后一行 */
+		for(;i<=(char*)(VGA_ADDR+2*screen_x_size*screen_y_size);i++)
+		{
+			*i=(char)0;
+		}
+		vga_addr=VGA_ADDR+2*screen_x_size*(screen_y_size-1);//显存指针在最后一行第一字符
+	}
+	/* 不为回车则打印当前字符 */
+	if(c!='\n')
+	{
+		char *p;
+		p=(char*)vga_addr;
+		*p=c;
+		p=(char*)vga_addr+1;
+		*p=(char)0x07;
+		vga_addr+=2;//一个字符占2-byte
+	}
 }
 /* 清屏 */
 void CleanupScreen()
