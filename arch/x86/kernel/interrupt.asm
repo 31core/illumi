a35753a0f122a7d17640e82465b67b50d95cedf1
orgofzero <zero.mail@foxmail.com>
2021-02-13 17:47:34 +0800

    Update arch\x86\kernel\interrupt.asm.

--- arch/x86/kernel/interrupt.asm
+++ arch/x86/kernel/interrupt.asm
@@ -2,17 +2,7 @@ global asm_interrupt21h
 extern interrupt21h
 
 asm_interrupt21h:
-	push es
-	push ds
 	pushad
-	mov eax,esp
-	push eax
-	mov ax,ss
-	mov ds,ax
-	mov es,ax
 	call interrupt21h
-	pop eax
 	popad
-	pop ds
-	pop es
 	iretd
