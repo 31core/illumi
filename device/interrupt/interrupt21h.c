a0d6d63218195d2cb0cd4e7ccc4d52e6ce98e2a2
orgofzero <zero.mail@foxmail.com>
2021-02-19 11:09:36 +0800

    优化了键盘布局.

--- device/interrupt/interrupt21h.c
+++ device/interrupt/interrupt21h.c
@@ -12,12 +12,12 @@ void interrupt21h(int *esp)
 	io_out8(0x20,0x61);//通知PIC已经发生中断
 	unsigned char data=io_in8(0x60);//读取键盘数据
 	/* shift抬起 */
-	if(data==0xaa)
+	if(data==0xaa||data==0xb6)
 	{
 		key_shift=1;
 	}
 	/* shift按下 */
-	else if(data==0x2a)
+	else if(data==0x2a||data==0x36)
 	{
 		key_shift=2;
 	}
