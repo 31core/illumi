3f5c6d8e0a15feac626c737c2dff07fd8d2c943a
orgofzero <zero.mail@foxmail.com>
2021-05-09 19:25:05 +0800

    增加了cpuid功能. 修复kernel/types.c中的错误.

---
+++ include/arch/x86/cpuid.h
@@ -0,0 +1,2 @@
+void cpuid_get_vendorid(char*);
+void cpuid_get_brand(char*);
--- kernel/types.c
+++ kernel/types.c
@@ -11,7 +11,8 @@ void uint2str(char *str, unsigned int num)
 	while(1)
 	{
 		/* 最低位为0 */
-		if(num % check == 0)
+		/* i+num=0则num最高位为0 */
+		if(num % check == 0 || i + num == 0)
 		{
 			str0[i] = j + 48;
 			check *= 10; //最低位为个位、十位以此类推
@@ -25,7 +26,15 @@ void uint2str(char *str, unsigned int num)
 		else
 		{
 			j += 1;
-			num -= check / 10;
+			/* 检测最高为时check会溢出 */
+			if(i == 0)
+			{ 
+				num -= 1e9;
+			}
+			else
+			{
+				num -= check / 10;
+			}
 		}
 	}
 	str_cpy(str, str0 + i + 1);
@@ -34,43 +43,17 @@ void uint2str(char *str, unsigned int num)
 void int2str(char *str, int num)
 {
 	char str0[11];
-	int check = 10; //用于检测最低位是否为0
-	int i, j;
-	i = 10; //用于从后到前写入str0字符
-	j = 0; //用于记录最低位的大小
-	if(num < 0)
+	if(num >= 0)
 	{
-		str0[i] = '-';
-		i -= 1;
-		num = -num;
+		uint2str(str, num);
 	}
-	while(1)
+	else
 	{
-		/* 最低位为0 */
-		if(num % check == 0)
-		{
-			str0[i] = j + 48;
-			check *= 10; //最低位为个位、十位以此类推
-			i -= 1;
-			j = 0;
-			if(num == 0)
-			{
-				break;
-			}
-		}
-		else
-		{
-			j += 1;
-			num -= check / 10;
-		}
+		uint2str(str0, -num);
+		str[0] = '-';
+		str[1] = '\0';
+		str_cat(str, str0);
 	}
-	if(str0[10] == '-')
-	{
-		str0[10] = '\0';
-		str0[i] = '-';
-		i -= 1;
-	}
-	str_cpy(str, str0 + i + 1);
 }
 /* 字符串转无符号整形数字 */
 unsigned int str2uint(char *str)
@@ -96,30 +79,12 @@ unsigned int str2uint(char *str)
 /* 字符串转整形数字 */
 int str2int(char *str)
 {
-	unsigned int i = 0; //i用来累计str数值
-	int x, y, t;
-	x = 0; //用来从高到低取str
 	if(str[0] == '-')
 	{
-		x = 1;
+		return -str2uint(str + 1);
 	}
-	int j = str_len(str) - 1;
-	while(str[x] != '\0')
+	else
 	{
-		t = str[x] - 48; //获取那一位的数字
-		/* t = t * 10 ^ j */
-		for(y = 0; y < j; y++)
-		{
-			t *= 10;
-		}
-		i += t;
-		x += 1; //接下来取str的下一个数字
-		j -= 1;
+		return -str2uint(str + 1);
 	}
-	/* 取相反数 */
-	if(str[0] == '-')
-	{
-		i = -1;
-	}
-	return i;
 }
