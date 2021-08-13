61bf31f9075901c592da89c9e7ee6ab99b054cd7
31core <zero.mail@foxmail.com>
2021-08-13 12:12:58 +0800

    Update device/serial/io.c. 串口输入支持.

--- device/serial/io.c
+++ device/serial/io.c
@@ -1,12 +1,40 @@
 #include <arch/x86/asmfunc.h>
 
+/* 串口发送一个字符数据 */
+void serial_send_char(char c)
+{
+	io_out8(0x3f8, c);
+}
 /* 串口发送数据 */
 void serial_send(char *str)
 {
 	int i = 0;
 	while(str[i] != '\0')
 	{
-		io_out8(0x3f8, str[i]);
+		serial_send_char(str[i]);
+		i += 1;
+	}
+}
+/* 串口接收一个字符数据 */
+char serial_recv_char()
+{
+	while((io_in8(0x3f8 + 5) & 1) == 0);
+	return io_in8(0x3f8);
+}
+/* 串口发送数据 */
+void serial_recv(char *ret)
+{
+	int i = 0;
+	while(1)
+	{
+		ret[i] = serial_recv_char();
+		serial_send_char(ret[i]);
+		/* 回车 */
+		if(ret[i] == '\r')
+		{
+			ret[i] = '\0';
+			return;
+		}
 		i += 1;
 	}
 }
--- include/device/serial/io.h
+++ include/device/serial/io.h
@@ -1,1 +1,4 @@
 void serial_send(char*);
+void serial_send_char(char);
+void serial_recv(char*);
+char serial_recv_char();
